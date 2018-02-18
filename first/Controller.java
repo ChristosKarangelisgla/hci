69fb3888e33753bf477b5823a9fa72deabb5de5e
ChristosKarangelisgla <2345191k@student.gla.ac.uk>
2018-02-18 18:52:28 +0000

    first view

---
+++ first/Controller.java
@@ -0,0 +1,24 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import ViewPack.View;
+
+public class Controller implements ActionListener{
+	
+	View view;
+	
+	public Controller () {
+		
+		view = new View();
+		
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
